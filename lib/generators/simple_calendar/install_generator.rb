27be50143695f47ffddec3cf8d1a1f8b24cc88d6
Rodrigo Argumedo <rodrigo.argumedo@icloud.com>
2015-07-13 19:46:01 -0500

    Add install generator

---
+++ lib/generators/simple_calendar/install_generator.rb
@@ -0,0 +1,9 @@
+require 'rails/generators'
+
+module SimpleCalendar
+    module Generators
+        class InstallGenerator < Rails::Generators::Base
+            # TODO: Add core installation stuff to this file
+        end
+    end
+end
---
+++ spec/install_generator_spec.rb
@@ -0,0 +1,8 @@
+require "spec_helper"
+require "generators/simple_calendar/install_generator"
+
+describe SimpleCalendar::Generators::InstallGenerator do
+    it 'installs the necessary install files'
+    it 'destroys the install files'
+    it 'should verify the contents'
+end
